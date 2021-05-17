9feabf9587ba79287fa14bffabfb8a2bf1fceff9
iboyko21 <80732145+iboyko21@users.noreply.github.com>
2021-05-17 10:58:39 -0700

    Add files via upload

---
+++ taskManager/scripts/task.js
@@ -0,0 +1,13 @@
+class Task {
+  // class names start with capital letter in JS
+  constructor(title, desc, important, dueDate, alertText, location) {
+    this.title = title;
+    this.description = desc;
+    this.important = important;
+    this.dueDate = dueDate;
+    this.alertText = alertText;
+    this.location = location;
+
+    this.user = "Igor";
+  }
+}
